959e58ffed856a81b4eacd0640853f49203103af
yoouali <youssefoualifessuoy@gmail.com>
2021-07-17 13:10:46 +0100

    set web server

---
+++ html/index.php
@@ -0,0 +1,27 @@
+<?php
+$host = "mysql-server";
+$user = "root";
+$pass = "secret";
+$db = "camagrsfdsu";
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+</body>
+
+</html>
---
+++ zbala/sql.sql
@@ -0,0 +1,12 @@
+# users table
+
+CREATE TABLE USERS(
+	id int AUTO_INCREMENT,
+    fullname VARCHAR(255),
+    email VARCHAR(100),
+    username VARCHAR(100),
+    password VARCHAR(100),
+    avatar VARCHAR(255)
+)
+
+
