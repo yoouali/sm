547154ef65cd83cbaa7a593927e90d5445f328dd
yoouali <youssefoualifessuoy@gmail.com>
2021-07-17 17:14:06 +0100

    link botstrap

--- html/index.php
+++ html/index.php
@@ -7,7 +7,7 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    die("ERROR: Could not connect. " . $e->getMessage());
 }
 ?>
 
