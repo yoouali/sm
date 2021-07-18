d9eaa49334a5412031f4615b7966e4a9bbeb3332
yoouali <youssefoualifessuoy@gmail.com>
2021-07-18 15:25:16 +0100

    link database with mysql pdo

--- html/index.php
+++ html/index.php
@@ -1,100 +1,6 @@
 <?php
-//link database
 include './config.php';
-// require_once "config.php";
-$link = mysqli_connect('mysql-server', 'root', 'secret', 'camagru');
-
-//Define variables and initlize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
-
-// Processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // Validate username
-    if (empty(trim($_POST["username"]))) {
-        $username_err = "Please enter a username.";
-    } elseif (!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))) {
-        $username_err = "Username can only contain letters, numbers, and underscores.";
-    } else {
-        // Prepare a select statement
-        $sql = "SELECT id FROM USERS WHERE username = ?";
-
-        if ($stmt = mysqli_prepare($link, $sql)) {
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-
-            // Set parameters
-            $param_username = trim($_POST["username"]);
-
-            // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-                /* store result */
-                mysqli_stmt_store_result($stmt);
-
-                if (mysqli_stmt_num_rows($stmt) == 1) {
-                    $username_err = "This username is already taken.";
-                } else {
-                    $username = trim($_POST["username"]);
-                }
-            } else {
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-
-    // Validate password
-    if (empty(trim($_POST["password"]))) {
-        $password_err = "Please enter a password.";
-    } elseif (strlen(trim($_POST["password"])) < 6) {
-        $password_err = "Password must have atleast 6 characters.";
-    } else {
-        $password = trim($_POST["password"]);
-    }
-
-    // Validate confirm password
-    if (empty(trim($_POST["confirm_password"]))) {
-        $confirm_password_err = "Please confirm password.";
-    } else {
-        $confirm_password = trim($_POST["confirm_password"]);
-        if (empty($password_err) && ($password != $confirm_password)) {
-            $confirm_password_err = "Password did not match.";
-        }
-    }
-
-    // Check input errors before inserting in database
-    if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
-
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-
-        if ($stmt = mysqli_prepare($link, $sql)) {
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-
-            // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-
-            // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-                // Redirect to login page
-                header("location: login.php");
-            } else {
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-
-    // Close connection
-    mysqli_close($link);
-}
+require 'config.php';
 
 ?>
 
@@ -118,7 +24,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <form class="containerform" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group row">
                 <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputFullname" placeholder="Full Name" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10">
                     <input type="text" class="form-control" id="inputUsername" placeholder="User Name" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputEmail" placeholder="Email Address" required>
                 </div>
             </div>
             <div class="form-group row">
@@ -133,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
             <div class="form-group row">
                 <div class="col-sm-10 offset-sm-2">
-                    <button type="submit" class="btn btn-primary">Sign Up</button>
+                    <button type="submit" name="btn_regestration" class="btn btn-primary">Sign Up</button>
                 </div>
             </div>
         </form>
--- zbala/sql.sql
+++ zbala/sql.sql
@@ -1,12 +1,18 @@
 # users table
-
 CREATE TABLE USERS(
-	id int AUTO_INCREMENT,
+    id int AUTO_INCREMENT,
     fullname VARCHAR(255),
     email VARCHAR(100),
     username VARCHAR(100),
     password VARCHAR(100),
     avatar VARCHAR(255)
-)
-
-
+) ## create table with null value
+CREATE TABLE USERS (
+    id int NOT NULL AUTO_INCREMENT,
+    profileimg VARCHAR(100) NOT NULL,
+    fullname VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
